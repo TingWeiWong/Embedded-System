3777c1c10d5eea243311a14d4ac95d3bb7c88ab4
Andrew <andrew.wong.0101@gmail.com>
2019-10-04 15:23:59 +0800

    Add exe

--- client_socket.c
+++ client_socket.c
@@ -1,11 +1,13 @@
 /* A simple client program to interact with the myServer.c program on the Raspberry.
 myClient.c
-The port number used in 51717.
+The port number used in 8083.
 This code is compiled and run on the Macbook laptop as follows:
-   
+
+
     g++ -o myClient myClient.c 
     ./myClient
 
+Addapted from https://www.youtube.com/watch?v=LtXEMwSG5-8&t=1404s
 
 */
 #include <stdio.h>
@@ -25,71 +27,150 @@ void error(char *msg) {
     exit(0);
 }
 
-void sendData( int sockfd, int x ) {
-  int n;
+// void sendData( int sockfd, int x ) {
+//   int n;
 
+//   char buffer[32];
+//   sprintf( buffer, "%d\n", x );
+//   if ( (n = write( sockfd, buffer, strlen(buffer) ) ) < 0 )
+//       error( const_cast<char *>( "ERROR writing to socket") );
+//   buffer[n] = '\0';
+// }
+
+void sendArray(int network_socket, int unsorted[]){
   char buffer[32];
-  sprintf( buffer, "%d\n", x );
-  if ( (n = write( sockfd, buffer, strlen(buffer) ) ) < 0 )
-      error( const_cast<char *>( "ERROR writing to socket") );
-  buffer[n] = '\0';
+  int write_status;
+  sprintf(buffer,"%d\n", *unsorted);
+
+  write_status = write(network_socket, buffer, strlen(buffer));
+
+  // if (write_status == -1){
+  //   error(const_cast<char *> ("Error sending data!"));
+  // }
+  buffer[write_status] = '\0';
 }
 
-int getData( int sockfd ) {
-  char buffer[32];
-  int n;
 
-  if ( (n = read(sockfd,buffer,31) ) < 0 )
-       error( const_cast<char *>( "ERROR reading from socket") );
-  buffer[n] = '\0';
-  return atoi( buffer );
+int getArray(int network_socket){
+  char buffer[32];
+  int read_status;
+
+  read_status = read(network_socket,buffer,31);
+  // if (read_status == -1){
+  //   error (const_cast<char *> ("Error reading data!"));
+  // }
+  buffer[read_status] = '\0';
+  return atoi(buffer);
 }
+
+int getInt(int network_socket){
+  char buffer[32];
+  int read_status;
+
+  read_status = read(network_socket,buffer,31);
+  // if (read_status == -1){
+  //   error (const_cast<char *> ("Error reading data!"));
+  // }
+  buffer[read_status] = '\0';
+  return atoi(buffer);
+}
+
+void sendInt(int network_socket, int data){
+  char buffer[32];
+  int write_status;
+  sprintf(buffer,"%d\n", data);
+
+  write_status = write(network_socket, buffer, strlen(buffer));
+
+  // if (write_status == -1){
+  //   error(const_cast<char *> ("Error sending data!"));
+  // }
+  buffer[write_status] = '\0';
+}
+
+
+
+// int getData( int sockfd ) {
+//   char buffer[32];
+//   int n;
+
+//   if ( (n = read(sockfd,buffer,31) ) < 0 )
+//        error( const_cast<char *>( "ERROR reading from socket") );
+//   buffer[n] = '\0';
+//   return atoi( buffer );
+// }
 
 int main(int argc, char *argv[])
 {
-    int sockfd, portno = 51717, n;
 
     // Create socket
-    int network_socket = socket(AF_INET,SOCK_STREAM,0)
+    int network_socket = socket(AF_INET,SOCK_STREAM,0);
+    int portno = 8083;
 
+    // Assign address
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(portno);
+    server_address.sin_addr.s_addr = INADDR_ANY;
 
+    //Check connection 
 
+    int connect_result = connect(network_socket, (struct sockaddr *)& server_address, sizeof(server_address));
 
-
-
-
-
-    char serverIp[] = "169.254.0.2";
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
-    char buffer[256];
-    int data;
-
-    if (argc < 3) {
-      // error( const_cast<char *>( "usage myClient2 hostname port\n" ) );
-      printf( "contacting %s on port %d\n", serverIp, portno );
-      // exit(0);
+    if (connect_result == -1){
+      printf("Error connecting to server\n");
     }
-    if ( ( sockfd = socket(AF_INET, SOCK_STREAM, 0) ) < 0 )
-        error( const_cast<char *>( "ERROR opening socket") );
 
-    if ( ( server = gethostbyname( serverIp ) ) == NULL ) 
-        error( const_cast<char *>("ERROR, no such host\n") );
-    
-    bzero( (char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy( (char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
-    serv_addr.sin_port = htons(portno);
-    if ( connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
-        error( const_cast<char *>( "ERROR connecting") );
+    int n;
 
-    for ( n = 0; n < 10; n++ ) {
-      sendData( sockfd, n );
-      data = getData( sockfd );
-      printf("%d ->  %d\n",n, data );
-    }
-    sendData( sockfd, -2 );
+    sendInt(network_socket, 10);
 
-    close( sockfd );
+    int result;
+
+    result = getInt(n);
+
+
+    // sorted = getArray(network_socket);
+
+    // printf("%d -> %d \n", *unsorted, *sorted);
+
+
+    close(network_socket);
     return 0;
+
 }
+
+
+
+//     char serverIp[] = "169.254.0.2";
+//     char buffer[256];
+//     int data;
+
+//     if (argc < 3) {
+//       // error( const_cast<char *>( "usage myClient2 hostname port\n" ) );
+//       printf( "contacting %s on port %d\n", serverIp, portno );
+//       // exit(0);
+//     }
+//     if ( ( sockfd = socket(AF_INET, SOCK_STREAM, 0) ) < 0 )
+//         error( const_cast<char *>( "ERROR opening socket") );
+
+//     if ( ( server = gethostbyname( serverIp ) ) == NULL ) 
+//         error( const_cast<char *>("ERROR, no such host\n") );
+    
+//     bzero( (char *) &serv_addr, sizeof(serv_addr));
+//     serv_addr.sin_family = AF_INET;
+//     bcopy( (char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+//     serv_addr.sin_port = htons(portno);
+//     if ( connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
+//         error( const_cast<char *>( "ERROR connecting") );
+
+//     for ( n = 0; n < 10; n++ ) {
+//       sendData( sockfd, n );
+//       data = getData( sockfd );
+//       printf("%d ->  %d\n",n, data );
+//     }
+//     sendData( sockfd, -2 );
+
+//     close( sockfd );
+//     return 0;
+// }
